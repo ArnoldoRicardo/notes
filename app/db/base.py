32096a432da99692537d9be56f27d08e91b7b415
ArnoldoRicardo <anyback@gmail.com>
2022-04-13 09:13:18 -0500

    add notes

--- app/db/base.py
+++ app/db/base.py
@@ -1,2 +1,3 @@
-from app.db.base_class import Base
-from app.models import User
+# Note: we need import here the other models to be correctle maped for alembic
+from app.db.base_class import Base  # nq
+from app.models import User  # nq
